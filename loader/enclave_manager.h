3ed264711c43e1d600176a1bd9f4be1ab2ee55b4
Zixiong Liu <liuzixiong@gmail.com>
2019-02-15 13:57:12 -0600

    modify enclave manager interface

--- loader/enclave_manager.h
+++ loader/enclave_manager.h
@@ -5,12 +5,19 @@
 #include <sgx_arch.h>
 #include <sgx_user.h>
 
+struct EnclaveThreadHandle {
+    void *tcs;
+};
+
 class EnclaveManager {
 private:
     void *enclaveBase;
     size_t enclaveMemoryLen;
 public:
     EnclaveManager(void *base, size_t len);
+    void addPages(void *dest, void *src, size_t len);
+    EnclaveThreadHandle *createThread(void *entry);
+    void makeHeap(void *base, size_t len);
 };
 
 
