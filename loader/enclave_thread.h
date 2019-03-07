d3cb8a6c0ccb278b558cda726247bbb94b9ce3dc
kuroiiwa <zhangkaige96@gmail.com>
2019-03-07 04:15:09 +0000

    lock free queue 1st commit

--- loader/enclave_thread.h
+++ loader/enclave_thread.h
@@ -2,6 +2,7 @@
 #define ENCLAVE_THREAD_H
 
 #include <sgx_arch.h>
+#include <atomic>
 
 struct EnclaveThread {
     vaddr stack;
@@ -11,5 +12,113 @@ struct EnclaveThread {
 };
 
 extern "C" int __eenter(vaddr tcs, vaddr stack);
+
+
+template<typename T>
+struct DPointer {
+  public:
+	union {
+		uint64_t ui[2];
+		struct {
+			T* ptr;
+			uint64_t count;
+		} __attribute__ (( __aligned__( 16 ) ));
+	};
+
+	DPointer() : ptr(nullptr), count(0) {}
+	DPointer(T* p) : ptr(p), count(0) {}
+	DPointer(T* p, uint64_t c) : ptr(p), count(c) {}
+
+	bool cas(DPointer const& nval, DPointer const& cmp) {
+		bool result;
+		__asm__ __volatile__ (
+			"lock cmpxchg16b %1\n\t"
+			"setz %0\n"
+			: "=q" ( result )
+			,"+m" ( ui )
+			: "a" ( cmp.ptr ), "d" ( cmp.count )
+			,"b" ( nval.ptr ), "c" ( nval.count )
+			: "cc"
+		);
+		return result;
+	}
+
+	// We need == to work properly
+	bool operator==(DPointer const&x) {
+		return x.ptr == ptr && x.count == count;
+	}
+};
+
+template<typename T>
+class Queue
+{
+	struct Node;
+	typedef DPointer<Node> Pointer;
+
+	struct Node {
+		T value;
+		Pointer next;
+
+		Node() : next(nullptr) {}
+		Node(T x, Node* nxt) : value(x), next(nxt) {}
+	};
+
+	Pointer Head, Tail;
+	std::atomic<int> len;
+  public:
+	Queue() {
+		Node *node = new Node();
+		Head.ptr = Tail.ptr = node;
+	}
+
+	int getLen() const {return this->len;}
+	void push(T x);
+	bool take(T& pvalue);
+};
+
+template<typename T>
+void Queue<T>::push(T x) {
+	Node *node = new Node(x, nullptr);
+	Pointer tail, next;
+	do {
+		tail = Tail;
+		next = tail.ptr->next;
+		if (tail == Tail) {
+			if (next.ptr == nullptr) {
+				if (tail.ptr->next.cas(Pointer(node, next.count+1), next))
+					break;
+			} else {
+				Tail.cas(Pointer(next.ptr, tail.count+1), tail);
+			}
+		}
+	} while (true);
+	Tail.cas(Pointer(node, tail.count+1), tail);
+	this->len++;
+}
+
+template<typename T>
+bool Queue<T>::take(T& pvalue) {
+	Pointer head, tail, next;
+	do {
+		head = Head;
+		tail = Tail;
+		next = head.ptr->next;
+		if (head == Head) {
+			if (head.ptr == tail.ptr) {
+				if (next.ptr == nullptr)
+					return false;
+				Tail.cas(Pointer(next.ptr, tail.count+1), tail);
+			} else {
+				pvalue = next.ptr->value;
+				if (Head.cas(Pointer(next.ptr, head.count+1), head))
+					break;
+			}
+		}
+	} while (true);
+	delete(head.ptr);
+	this->len--;
+	return true;
+}
+
 
 #endif
