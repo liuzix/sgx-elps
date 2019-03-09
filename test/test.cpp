0f5ffe230ef2a1bd70c9cfb7080bd9c601964d1c
kuroiiwa <zhangkaige96@gmail.com>
2019-03-09 01:55:38 +0000

    test initial commit

--- test/test.cpp
+++ test/test.cpp
@@ -1,39 +1,46 @@
-#include <iostream>
-#include <vector> // vector class-template definition
+#include <gtest/gtest.h>
+#include <vector>
 #include <thread>
 #include <chrono>
-using namespace std;
+#include <atomic>
 
-#include "test.h"
+#include "queue.h"
+#include "spin_lock.h"
 
+namespace {
 
-Queue<int> *ReqQueue = new Queue<int>();
-atomic<int> sum(0);
-
-void insert(int val) {
-	ReqQueue->push(val);
-	this_thread::sleep_for(chrono::seconds(1));
+static void pushAndTake(Queue<int>& q, int val, std::atomic<int>& sum) {
+	q.push(val);
+	std::this_thread::sleep_for(std::chrono::seconds(1));
 	int tmp = 0;
-	if (ReqQueue->take(tmp))
+	if (q.take(tmp))
 		sum += tmp;
 }
 
+TEST(QueueTest, PushAndTake) {
+	Queue<int> IntQueue;
+	std::atomic<int> sum_g(0);
 
-int main()
-{
-	int sum_test = 0;
-	vector<thread> threads;
+	int sum_true = 0;
+	std::vector<std::thread> threads;
 
 	for (int i = 0; i < 50; i++) {
-		threads.push_back(thread(insert,i));
-		sum_test += i;
+		threads.push_back(std::thread(pushAndTake,
+				std::ref(IntQueue), i, std::ref(sum_g)));
+		sum_true += i;
 	}
 
 	for (auto& th : threads)
 		th.join();
 
-	cout << sum << ' ' << sum_test << endl;
+	EXPECT_TRUE(sum_true == sum_g);
+}
 
-	return 1;
-} // end main
+}
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
 
