c46d2953b5cbff0463b1d4430120019fafb81988
kuroiiwa <zhangkaige96@gmail.com>
2019-03-15 01:12:00 +0000

    another implementation

--- include/queue.h
+++ include/queue.h
@@ -7,52 +7,48 @@
 template<typename T>
 class Queue {
   private :
-	std::atomic<uint64_t> r_head;
     std::atomic<uint64_t> head;
-	std::atomic<uint64_t> r_tail;
 	std::atomic<uint64_t> tail;
   	std::vector<T> q;
 	uint64_t size;
-	uint64_t t() {
-		return static_cast<uint64_t>(tail);
+ 	uint64_t increment(uint64_t n) {
+		return (n + 1) % size;
 	}
-	uint64_t h() {
-		return static_cast<uint64_t>(head);
-	}
- 
  
    public :
-	Queue() : r_head(0),
-                        head(0),
-                        r_tail(0),
+	Queue() :           head(0),
                         tail(0),
 						q(10),
 						size(10) { }
-	Queue(uint64_t s) : r_head(0),
-                        head(0),
-                        r_tail(0),
+	Queue(uint64_t s) : head(0),
                         tail(0),
 						q(s),
 						size(s) { }
 	
-	// uses move constructor to avoid locked non-trivial copy
-	void push(T v) {
-		/* Find a place to write */
-		uint64_t pos = r_tail++;
-		while (h() + size <=  pos);
-		/* do the write */
-		q[pos % size] =std:: move(v);
-		/* stay you did the write */
-		++tail;
+	bool try_push(T& v) {
+		uint64_t curr_tail = tail.load();
+		uint64_t next_tail = increment(curr_tail);
+		if (next_tail != head.load()) {
+			q[curr_tail] = std::move(v);
+			tail.store(next_tail);
+			return true;
+		}
+		return false;
+	}
+	void push(T& v) {
+		while (!try_push(v));
 	}
 	
+	bool try_take(T& v) {
+		uint64_t curr_head = head.load();
+		if (curr_head == tail.load())
+			return false;
+		v = q[curr_head];
+		head.store(increment(curr_head));
+		return true;
+	}
 	void take(T & v) {
-		// choose a position to read from
-		uint64_t  pos = r_head++;
-		// don't read from the tail...
-		while (pos >= t());
-		v = std::move(q[pos%size]);
-		++head;
+		while (!try_take(v));
 	}
 };
 
